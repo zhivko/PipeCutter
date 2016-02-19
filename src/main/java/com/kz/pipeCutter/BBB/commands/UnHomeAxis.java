9c3d44c05b9eab7b687709981a6e7934023d0e99
zhivko <klemen.zivkovic@gmail.com>
2016-02-19 12:47:43 +0100

    Fixed unhoming.

--- src/main/java/com/kz/pipeCutter/BBB/commands/UnHomeAxis.java
+++ src/main/java/com/kz/pipeCutter/BBB/commands/UnHomeAxis.java
@@ -1,9 +1,6 @@
 package com.kz.pipeCutter.BBB.commands;
 
-import com.kz.pipeCutter.ui.Settings;
-
 import pb.Message.Container;
-import pb.Status.EmcTaskModeType;
 import pb.Types.ContainerType;
 
 public class UnHomeAxis extends MachineTalkCommand {
@@ -18,7 +15,7 @@ public class UnHomeAxis extends MachineTalkCommand {
 		pb.Message.Container.Builder builder = Container.newBuilder();
 		pb.Status.EmcCommandParameters emcCommandParameter = pb.Status.EmcCommandParameters.newBuilder().setIndex(axisNo)
 				.build();
-		builder.setType(ContainerType.MT_EMC_AXIS_INIT);
+		builder.setType(ContainerType.MT_EMC_AXIS_UNHOME);
 		builder.setEmcCommandParams(emcCommandParameter);
 		builder.setTicket(ticket++);
 		Container container = builder.build();

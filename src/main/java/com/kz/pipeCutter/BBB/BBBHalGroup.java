45d8c72513dadcf93506af0ebab1dc3a06ae5617
zhivko <klemen.zivkovic@gmail.com>
2016-06-13 23:55:00 +0200

    Trying to make MT_HALRCOMMAND_DESCRIBE work. Seems like bug: https://github.com/machinekit/machinekit/issues/968

--- src/main/java/com/kz/pipeCutter/BBB/BBBHalGroup.java
+++ src/main/java/com/kz/pipeCutter/BBB/BBBHalGroup.java
@@ -1,13 +1,9 @@
 package com.kz.pipeCutter.BBB;
 
-import java.io.ByteArrayInputStream;
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
-import org.zeromq.ZMQ.Context;
 import org.zeromq.ZMQ.PollItem;
 import org.zeromq.ZMQ.Poller;
 import org.zeromq.ZMQ.Socket;
@@ -22,7 +18,7 @@ import com.kz.pipeCutter.ui.Settings;
 
 public class BBBHalGroup implements Runnable {
 	Random rand = new Random(23424234);
-	static Socket client=null;
+	static Socket client = null;
 
 	public static void main(String[] args) {
 
@@ -32,56 +28,59 @@ public class BBBHalGroup implements Runnable {
 		BBBHalGroup halGroup = new BBBHalGroup();
 		Thread myThread = new Thread(halGroup);
 		myThread.start();
-		
+
 		pb.Message.Container.Builder builder = Container.newBuilder();
 
-		builder.setType(ContainerType.MT_HALRCOMMAND_DESCRIBE);
-		// builder.setTicket(MachineTalkCommand.getNextTicket());
+		//builder.setType(ContainerType.MT_HALRCOMMAND_DESCRIBE);
+		builder.setType(ContainerType.MT_PING);
+		//builder.setTicket(MachineTalkCommand.getNextTicket());
 
 		Container container = builder.build();
+
 		byte[] buff = container.toByteArray();
 		String hexOutput = javax.xml.bind.DatatypeConverter.printHexBinary(buff);
 		System.out.println("Message: " + hexOutput);
-		client.send(buff, 0);		
-	}	
-	
-	
-  public void run() {
-      ZContext ctx = new ZContext();
-      client = ctx.createSocket(ZMQ.DEALER);
+		client.send(buff, 0);
+	}
 
-      //  Set random identity to make tracing easier
-      String identity = String.format("%04X-%04X", rand.nextInt(), rand.nextInt());
-      client.setIdentity(identity.getBytes(ZMQ.CHARSET));
-      String halCmdUrl = Settings.getInstance().getSetting(
-  				"machinekit_halCmdService_url");
-      client.connect(halCmdUrl);
+	public void run() {
+		ZContext ctx = new ZContext();
+		client = ctx.createSocket(ZMQ.DEALER);
 
-      PollItem[] items = new PollItem[] { new PollItem(client, Poller.POLLIN) };
+		// Set random identity to make tracing easier
+		String identity = String
+				.format("%04X-%04X", rand.nextInt(), rand.nextInt());
+		client.setIdentity(identity.getBytes(ZMQ.CHARSET));
+		String halCmdUrl = Settings.getInstance().getSetting(
+				"machinekit_halCmdService_url");
+		client.connect(halCmdUrl);
 
-      int requestNbr = 0;
-      while (!Thread.currentThread().isInterrupted()) {
-          //  Tick once per second, pulling in arriving messages
-          for (int centitick = 0; centitick < 100; centitick++) {
-              ZMQ.poll(items, 10);
-              if (items[0].isReadable()) {
-                  ZMsg msg = ZMsg.recvMsg(client);
-                  
-              		Container contReturned;
-              		try {
-              			contReturned = Message.Container.parseFrom(msg.getFirst().getData());
-              			Settings.instance.log(contReturned.toString());
-              		} catch (InvalidProtocolBufferException e) {
-              			// TODO Auto-generated catch block
-              			e.printStackTrace();
-              		}                  
-                  
-                  msg.getLast().print(identity);
-                  msg.destroy();
-              }
-          }
-          client.send(String.format("request #%d", ++requestNbr), 0);
-      }
-      ctx.destroy();
-  }
+		PollItem[] items = new PollItem[] { new PollItem(client, Poller.POLLIN) };
+
+		int requestNbr = 0;
+		while (!Thread.currentThread().isInterrupted()) {
+			// Tick once per second, pulling in arriving messages
+			for (int centitick = 0; centitick < 4; centitick++) {
+				ZMQ.poll(items, 10);
+				if (items[0].isReadable()) {
+					ZMsg msg = ZMsg.recvMsg(client);
+
+					Container contReturned;
+					try {
+						contReturned = Message.Container
+								.parseFrom(msg.getFirst().getData());
+						Settings.instance.log(contReturned.toString());
+					} catch (InvalidProtocolBufferException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					msg.getLast().print(identity);
+					msg.destroy();
+				}
+			}
+
+		}
+		ctx.destroy();
+	}
 }

f605f437687bd6254387208a931bed4021e61242
Zhivko <klemen.zivkovic@gmail.com>
2017-02-13 22:53:13 +0100

    new file:   screenshots/PipeCutterVideo.gif 	new file:   screenshots/convertVideoToGif.sh 	new file:   screenshots/palette.png

---
+++ screenshots/convertVideoToGif.sh
@@ -0,0 +1,7 @@
+#################################
+##  script to create gif from mp4
+#################################
+#Generate a palette
+ffmpeg -y -ss 30 -t 3 -i ./screenshots/PlasmaPipeCutterUI.mp4 -vf fps=10,scale=320:-1:flags=lanczos,palettegen palette.png
+#convert to gif
+ffmpeg -ss 30 -t 200 -i ./screenshots/PlasmaPipeCutterUI.mp4 -i palette.png -filter_complex "fps=10,scale=1024:-1:flags=lanczos[x];[x][1:v]paletteuse" -fs 10485760 PipeCutterVideo.gif
